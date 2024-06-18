beb19ebb0242ecce58afd860be9508e78313eb5f
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-06-18 22:13:16 +0000

    Tue, Jun 18, 2024, 10:13 PM +00:00

--- me/hello.c
+++ me/hello.c
@@ -2,17 +2,21 @@
 #include <string.h>
 
 int main() {
+    //array for name
     char name[100];
 
+    //get name
     printf("What's your name? ");
     fgets(name, sizeof(name), stdin);
 
+    //remove newline
     size_t length = strlen(name);
     if (length > 0 && name[length - 1] == '\n') {
         name[length - 1] = '\0';
     }
 
-    printf("Hello, %s!\n", name);
+    //print name
+    printf("hello, %s\n", name);
 
     return 0;
 }
