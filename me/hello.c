504f57da88445c35f74dfe0eb8612c1e1c94550d
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-06-18 22:06:52 +0000

    Tue, Jun 18, 2024, 10:06 PM +00:00

--- me/hello.c
+++ me/hello.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main () {
-    char input[20];
-
-    scanf("What's your name? %s", &input)
+    char* input;
+    input = malloc( 20 * sizeof(char));
+    scanf("What's your name? %s", &input);
 
     printf("hello, %s\n", input);
 
