c12963d9f2a73797b45c0918b1a1b720a2233775
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-06-19 18:18:42 +0000

    Wed, Jun 19, 2024, 2:18 PM -04:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,28 +1,35 @@
-#include <stdio.h>
 #include <ctype.h>
+#include <stdio.h>
 
-int main() {
+int main()
+{
     int height;
     int temp;
     char tag = '#';
 
-    //ask for (correct) input
-    do {
+    // ask for (correct) input
+    do
+    {
         printf("Height: ");
         temp = scanf("%d", &height);
 
-        //clear input buffer on failure
-        while (getchar() != '\n');
+        // clear input buffer on failure
+        while (getchar() != '\n')
+            ;
+    }
+    while (height < 1 || temp != 1);
 
-    } while (height < 1 || temp != 1);
-
-
-    //print pyramid
-    for (int i= 0; i<height; i++) {
-        for (int j=1; j<=height; j++) {
-            if (j<height-i) {
+    // print pyramid
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 1; j <= height; j++)
+        {
+            if (j < height - i)
+            {
                 printf(" ");
-            } else {
+            }
+            else
+            {
                 printf("%c", tag);
             }
         }
