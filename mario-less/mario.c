c17728781a4520da6d284246a3ae147a08e74716
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-06-19 01:04:44 +0000

    Tue, Jun 18, 2024, 9:04 PM -04:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -2,12 +2,13 @@
 
 int main() {
     int height;
+    char tag = '#';
 
     printf("Height: ");
     scanf("%d", &height);
 
     for (int i= 0; i<height; i++) {
-        printf("%10d\n", i);
+        printf("%10c\n", tag);
     }
     return 0;
 }
