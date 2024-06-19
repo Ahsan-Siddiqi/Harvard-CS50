537889b32bd3912efe3e17055a03627dc8a78a4b
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-06-19 18:16:09 +0000

    Wed, Jun 19, 2024, 2:16 PM -04:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -6,15 +6,18 @@ int main() {
     int temp;
     char tag = '#';
 
-
+    //ask for (correct) input
     do {
         printf("Height: ");
         temp = scanf("%d", &height);
+
+        //clear input buffer on failure
         while (getchar() != '\n');
-        
+
     } while (height < 1 || temp != 1);
 
 
+    //print pyramid
     for (int i= 0; i<height; i++) {
         for (int j=1; j<=height; j++) {
             if (j<height-i) {
@@ -25,5 +28,6 @@ int main() {
         }
         printf("\n");
     }
+
     return 0;
 }
