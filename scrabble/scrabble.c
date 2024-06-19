ee21339739274bbde79c935f771c39788ac19a14
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-06-19 23:30:08 +0000

    Wed, Jun 19, 2024, 7:30 PM -04:00

---
+++ scrabble/scrabble.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+
+    char input1[100];
+    char input2[100];
+
+    // letter values
+    int a, e, i, l, n, o, r, s, t, u = 1;
+    int d, g = 2;
+    int b, c, m, p = 3;
+    int f, h, v, w, y = 4;
+    int k = 5;
+    int j, x = 8;
+    int q, z = 10;
+
+    // input
+    printf("Player 1: ");
+    scanf("%s", &input1);
+
+    printf("Player 2: ");
+    scanf("%s", &input2);
+
+
+    // winner
+    for (int i = 0; i < sizeof(input1); i++) {
+        switch (input1[i]) {
+            case i == 'a':
+                
+        }
+    }
+
+    return 0;
+}
