358e33666997232c281a07c4ceea0040d7f69b75
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-06-20 00:50:43 +0000

    Wed, Jun 19, 2024, 8:50 PM -04:00

--- readability/readability.c
+++ readability/readability.c
@@ -2,8 +2,21 @@
 #include <string.h>
 
 int main() {
+    char input[];
+    int L, S;
 
-    
+    // input
+    printf("Text: ");
+    fgets(input, sizeof(input), stdin);
+
+    size_t length = strlen(input);
+    if (length > 0 && input[length - 1] == '\n')
+    {
+        input[length - 1] = '\0';
+    }
+
+    // get avg letters/100 words and avg sentences/100 words
+    L = spaces 
 
     return 0;
 }
