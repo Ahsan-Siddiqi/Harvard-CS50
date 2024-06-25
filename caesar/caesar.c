d53b0a4a555f911e952270acf641abbdb09ae175
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-06-25 18:51:23 +0000

    Tue, Jun 25, 2024, 2:51 PM -04:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,22 +1,26 @@
-#include <stdio.h>
 #include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-int main (int argc, char** argv) {
+int main(int argc, char **argv)
+{
 
     char input[99999];
     int key;
 
     // grab arg input
-    if (argc != 2) {
+    if (argc != 2)
+    {
         printf("Usage: ./caesar key\n");
         exit(1);
     }
 
     // Validate that the key is a digit
-    for (int i = 0; argv[1][i] != '\0'; i++) {
-        if (!isdigit(argv[1][i])) {
+    for (int i = 0; argv[1][i] != '\0'; i++)
+    {
+        if (!isdigit(argv[1][i]))
+        {
             printf("Usage: ./caesar key\n");
             return 1;
         }
@@ -29,14 +33,17 @@ int main (int argc, char** argv) {
     fgets(input, sizeof(input), stdin);
 
     size_t length = strlen(input);
-    if (length > 0 && input[length - 1] == '\n') {
+    if (length > 0 && input[length - 1] == '\n')
+    {
         input[length - 1] = '\0';
         length--;
     }
 
     // manipulate input text
-    for (int i = 0; i < length; i++) {
-        if (isalpha(input[i])) {
+    for (int i = 0; i < length; i++)
+    {
+        if (isalpha(input[i]))
+        {
             char offset = isupper(input[i]) ? 'A' : 'a';
             input[i] = (input[i] - offset + key) % 26 + offset;
         }
