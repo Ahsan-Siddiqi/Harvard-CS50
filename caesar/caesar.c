9e07ceb1364488ff2faf6b1093d92c2efd5a3d31
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-06-25 18:12:17 +0000

    Tue, Jun 25, 2024, 2:12 PM -04:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -5,11 +5,20 @@ int main (int argc, char** argv) {
     char input[99999];
 
     // grab arg input
-    do {
+    if (argc != 2 || !isdigit(argv[2])) {
+        prtinf("Usage: ./caesar key");
+        break;
+    }
 
-        // grab input text
+    // grab input text
+    printf("plaintext: ");
+    fgets(input, sizeof(input), stdin);
 
-    } while (argc != 2 || isdigit(argv[2]));
+    size_t length = strlen(input);
+    if (length > 0 && input[length - 1] == '\n')
+    {
+        input[length - 1] = '\0';
+    }
 
     // manipulate input text
 
