87ab842be5f1082e455ceae899d8ae48d7cebcaf
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-06-25 18:04:16 +0000

    Tue, Jun 25, 2024, 2:04 PM -04:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -3,12 +3,11 @@
 int main (int argc, char** argv) {
 
     char input[99999];
-    
 
     // grab arg input
-
+    do {
         // grab input text
-
+    } while (argc != 2 || );
     // manipulate input text
 
     // print result
