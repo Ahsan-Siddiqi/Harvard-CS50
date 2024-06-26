074ccc80a1b1b4769bfda2237bd790f1eb27537f
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-06-26 15:46:50 +0000

    Wed, Jun 26, 2024, 11:46 AM -04:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -171,7 +171,7 @@ int find_min(void)
     int low = voter_count;
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].votes < voter_count)
+        if (candidates[i].votes < voter_count && !candidates[i].eliminated)
         {
             low = candidates[i].votes;
         }
