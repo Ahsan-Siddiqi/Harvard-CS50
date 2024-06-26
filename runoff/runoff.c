c23bee6b1d25e8eef477bc6c7a14fd3f02d560ad
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-06-26 15:38:07 +0000

    Wed, Jun 26, 2024, 11:38 AM -04:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -166,8 +166,15 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    // TODO
-    
+
+    int low = voter_count;
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes < voter_count)
+        {
+            low = candidates[i].votes;
+        }
+    }
 
     return 0;
 }
