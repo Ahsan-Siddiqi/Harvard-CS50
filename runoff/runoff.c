7370aed0da949b0928b1e8b330b2f41fcefe7725
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-06-26 15:06:30 +0000

    Wed, Jun 26, 2024, 11:06 AM -04:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -131,7 +131,7 @@ bool vote(int voter, int rank, string name)
     {
         if (!strcmp(candidates[i].name, name))
         {
-            candidates[i].votes++;
+            preferences[i]
             return true;
         }
     }
