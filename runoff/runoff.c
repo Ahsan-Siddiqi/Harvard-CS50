7e5e46c7c18cb39a0f4a5fbc913cc078e2d2e115
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-06-26 16:01:42 +0000

    Wed, Jun 26, 2024, 12:01 PM -04:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -202,7 +202,7 @@ bool is_tie(int min)
 
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].votes != min)
+        if (candidates[i].votes != min && !candidates[i].eliminated)
         {
             return false;
         }
