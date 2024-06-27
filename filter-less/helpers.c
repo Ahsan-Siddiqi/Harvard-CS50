2c8b8bfc1c9f393b92d7063a16eaa9c1d255c9b3
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-06-27 22:55:51 +0000

    Thu, Jun 27, 2024, 6:55 PM -04:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -69,14 +69,14 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             RGBTRIPLE* pixel = &image[i][j];
             RGBTRIPLE* blurredPixel = &blurred[i][j];
 
-            if (RGBTRIPLE* p1 = &image[i-1][j-1];)? continue : p1 = 0;
-            if (RGBTRIPLE* p2 = &image[i-1][j])? continue : p2 = 0;
-            if (RGBTRIPLE* p3 = &image[i-1][j+1])? continue : p3 = 0;
-            if (RGBTRIPLE* p4 = &image[i][j-1])? continue : p4 = 0;
-            if (RGBTRIPLE* p5 = &image[i][j+1])? continue : p5 = 0;
-            if (RGBTRIPLE* p6 = &image[i+1][j-1])? continue : p6 = 0;
-            if (RGBTRIPLE* p7 = &image[i+1][j])? continue : p7 = 0;
-            if (RGBTRIPLE* p8 = &image[i+1][j+1])? continue : p8 = 0;
+            RGBTRIPLE* p1 = &image[i-1][j-1];
+            RGBTRIPLE* p2 = &image[i-1][j];
+            RGBTRIPLE* p3 = &image[i-1][j+1];
+            RGBTRIPLE* p4 = &image[i][j-1];
+            RGBTRIPLE* p5 = &image[i][j+1];
+            RGBTRIPLE* p6 = &image[i+1][j-1];
+            RGBTRIPLE* p7 = &image[i+1][j];
+            RGBTRIPLE* p8 = &image[i+1][j+1];
 
             int avgRed = (p1->rgbtRed + p2->rgbtRed + p3->rgbtRed + p4->rgbtRed + p5->rgbtRed + p6->rgbtRed + p7->rgbtRed + p8->rgbtRed) / 8;
             int avgBlue = (p1->rgbtBlue + p2->rgbtBlue + p3->rgbtBlue + p4->rgbtBlue + p5->rgbtBlue + p6->rgbtBlue + p7->rgbtBlue + p8->rgbtBlue) / 8;
