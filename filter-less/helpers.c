02257bb4a7f2393e2ee301d82edaccdce021b272
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-06-27 23:42:49 +0000

    Thu, Jun 27, 2024, 7:42 PM -04:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -4,9 +4,11 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    for(int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            RGBTRIPLE* pixel = &image[i][j];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            RGBTRIPLE *pixel = &image[i][j];
             int avg = (pixel->rgbtRed + pixel->rgbtGreen + pixel->rgbtBlue + 1) / 3;
             pixel->rgbtRed = avg;
             pixel->rgbtBlue = avg;
@@ -20,9 +22,11 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
 
-    for(int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            RGBTRIPLE* pixel = &image[i][j];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            RGBTRIPLE *pixel = &image[i][j];
 
             int originalRed = pixel->rgbtRed;
             int originalGreen = pixel->rgbtGreen;
@@ -32,9 +36,12 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             int sepiaGreen = round(.349 * originalRed + .686 * originalGreen + .168 * originalBlue);
             int sepiaBlue = round(.272 * originalRed + .534 * originalGreen + .131 * originalBlue);
 
-            if (sepiaRed > 255) sepiaRed = 255;
-            if (sepiaGreen > 255) sepiaGreen = 255;
-            if (sepiaBlue > 255) sepiaBlue = 255;
+            if (sepiaRed > 255)
+                sepiaRed = 255;
+            if (sepiaGreen > 255)
+                sepiaGreen = 255;
+            if (sepiaBlue > 255)
+                sepiaBlue = 255;
 
             pixel->rgbtRed = sepiaRed;
             pixel->rgbtGreen = sepiaGreen;
@@ -47,8 +54,10 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width / 2; j++) {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width / 2; j++)
+        {
 
             RGBTRIPLE temp = image[i][j];
             image[i][j] = image[i][width - j - 1];
@@ -65,17 +74,22 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 
     RGBTRIPLE blurred[height][width];
 
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
             int redSum = 0, greenSum = 0, blueSum = 0;
             int count = 0;
 
-            for (int di = -1; di <= 1; di++) {
-                for (int dj = -1; dj <= 1; dj++) {
+            for (int di = -1; di <= 1; di++)
+            {
+                for (int dj = -1; dj <= 1; dj++)
+                {
                     int ni = i + di;
                     int nj = j + dj;
 
-                    if (ni >= 0 && ni < height && nj >= 0 && nj < width) {
+                    if (ni >= 0 && ni < height && nj >= 0 && nj < width)
+                    {
                         redSum += image[ni][nj].rgbtRed;
                         greenSum += image[ni][nj].rgbtGreen;
                         blueSum += image[ni][nj].rgbtBlue;
@@ -84,14 +98,16 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 }
             }
 
-            blurred[i][j].rgbtRed = round((float)redSum / count);
-            blurred[i][j].rgbtGreen = round((float)greenSum / count);
-            blurred[i][j].rgbtBlue = round((float)blueSum / count);
+            blurred[i][j].rgbtRed = round((float) redSum / count);
+            blurred[i][j].rgbtGreen = round((float) greenSum / count);
+            blurred[i][j].rgbtBlue = round((float) blueSum / count);
         }
     }
 
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
             image[i][j] = blurred[i][j];
         }
     }
