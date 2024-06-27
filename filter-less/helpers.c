39ddd0f56ce5b678e4dfccc0b8b44840d4ef7dd8
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-06-27 23:20:49 +0000

    Thu, Jun 27, 2024, 7:20 PM -04:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -6,7 +6,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     for(int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
             RGBTRIPLE* pixel = &image[i][j];
-            int avg = (pixel->rgbtRed + pixel->rgbtGreen + pixel->rgbtBlue) / 3;
+            int avg = (pixel->rgbtRed + pixel->rgbtGreen + pixel->rgbtBlue + 1) / 3;
             pixel->rgbtRed = avg;
             pixel->rgbtBlue = avg;
             pixel->rgbtGreen = avg;
