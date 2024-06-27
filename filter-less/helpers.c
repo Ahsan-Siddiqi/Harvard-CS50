e93735f06f55ac7c2c3fca86b54218aaaf65e824
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-06-27 19:21:22 +0000

    Thu, Jun 27, 2024, 3:21 PM -04:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,6 +3,14 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+
+    for(int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            image[height][width].;
+        }
+    }
+
+
     return;
 }
 
