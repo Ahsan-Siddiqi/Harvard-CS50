0f16b56274d037d950323692fdc216705fa3b204
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-06-28 04:33:19 +0000

    Fri, Jun 28, 2024, 12:33 AM -04:00

--- recover/recover.c
+++ recover/recover.c
@@ -21,8 +21,8 @@ int main(int argc, char *argv[])
         // iterate over chunks of 512 bytes or 1 block
     char buffer[512];
     size_t bytesRead;
-    char pattern[] = {0xff, 0xd8, 0xff}
-    
+    char pattern[] = {0xff, 0xd8, 0xff};
+
 
     while(bytesRead = fread(&buffer, 1, sizeof(buffer), forensic)) {
 
