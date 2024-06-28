0f1688f952ffda4d2147384fd1268e5b8844f90a
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-06-28 20:54:19 +0000

    Fri, Jun 28, 2024, 4:54 PM -04:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -50,6 +50,7 @@ person *create_family(int generations)
 
         // TODO: Set parent pointers for current person
         
+
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
     }
 
