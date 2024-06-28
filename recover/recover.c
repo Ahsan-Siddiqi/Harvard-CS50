8e110f0809d302f054d01bcab8c6d8e8827ba1c8
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-06-28 05:33:01 +0000

    Fri, Jun 28, 2024, 1:33 AM -04:00

--- recover/recover.c
+++ recover/recover.c
@@ -5,32 +5,38 @@
 int main(int argc, char *argv[])
 {
 
-    if (argc != 2) {
+    if (argc != 2)
+    {
         printf("Usage: ./recover [imagename]\n");
         exit(1);
     }
 
-    FILE* forensic = fopen(argv[1], "r");
+    FILE *forensic = fopen(argv[1], "r");
 
-    if (forensic == NULL) {
+    if (forensic == NULL)
+    {
         printf("Input file cannot be opened\n");
         exit(1);
     }
 
     // look through the raw for the jpeg pattern (0xff 0xd8 0xff)
 
-        // iterate over chunks of 512 bytes or 1 block
+    // iterate over chunks of 512 bytes or 1 block
     unsigned char buffer[512];
     size_t bytesRead;
     int nPics = 0;
     char picName[8];
-    FILE* output = NULL;
+    FILE *output = NULL;
 
-    while((bytesRead = fread(buffer, sizeof(unsigned char), sizeof(buffer), forensic)) > 0) {
+    while ((bytesRead = fread(buffer, sizeof(unsigned char), sizeof(buffer), forensic)) > 0)
+    {
 
         // when pattern match, open file to write to
-        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0) {
-            if(output != NULL) {
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff &&
+            (buffer[3] & 0xf0) == 0xe0)
+        {
+            if (output != NULL)
+            {
                 fclose(output);
             }
 
@@ -40,15 +46,16 @@ int main(int argc, char *argv[])
             fwrite(buffer, sizeof(unsigned char), bytesRead, output);
 
             nPics++;
-        } else if (output != NULL) {
+        }
+        else if (output != NULL)
+        {
 
             fwrite(buffer, sizeof(unsigned char), bytesRead, output);
         }
-
-
     }
 
-    if (output != NULL) {
+    if (output != NULL)
+    {
         fclose(output);
     }
     fclose(forensic);
