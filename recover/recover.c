b7a221230c262bcf120431fc69b7bd71e7ac3685
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-06-28 01:04:54 +0000

    Thu, Jun 27, 2024, 9:04 PM -04:00

--- recover/recover.c
+++ recover/recover.c
@@ -12,5 +12,7 @@ int main(int argc, char *argv[])
     // look through the raw for the jpeg pattern (0xff 0xd8 0xff)
 
         // iterate over chunks of 512 bytes or 1 block
+
+            // when pattern match, open file to write to
     return 0;
 }
