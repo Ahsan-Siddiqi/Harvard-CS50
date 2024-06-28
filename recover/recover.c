d0610f57715aed87a5e3051cc545c9d162fbd239
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-06-28 04:30:31 +0000

    Fri, Jun 28, 2024, 12:30 AM -04:00

--- recover/recover.c
+++ recover/recover.c
@@ -22,13 +22,13 @@ int main(int argc, char *argv[])
         // iterate over chunks of 512 bytes or 1 block
     char buffer[512];
     size_t bytesRead;
-    char pattern[] = {0xff, 0xd8, 0xff, }
+    char pattern[] = {0xff, 0xd8, 0xff}
 
     while(bytesRead = fread(&buffer, 1, sizeof(buffer), forensic)) {
 
         // when pattern match, open file to write to
         if (bytesRead >= 4 && !memcmp(buffer, pattern, 3)) {
-            
+            fopen()
         }
         // copy contents until another pattern found
 
