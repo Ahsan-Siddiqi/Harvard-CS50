6b8ed59fec491f07370d79f910bd60d29e788298
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-06-28 21:18:19 +0000

    Fri, Jun 28, 2024, 5:18 PM -04:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -77,9 +77,11 @@ void free_family(person *p)
 {
 
     // TODO: Handle base case
-
+    if (p->parents[0] == NULL) {
+        free(p);
+    } else {
     // TODO: Free parents recursively
-    if(p->parents[0] != NULL) {
+
         free_family(person *p->parents[0])
     }
     // TODO: Free child
