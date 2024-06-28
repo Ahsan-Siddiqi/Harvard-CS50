6997ef673504b53df0e25fcca087fd4c9a393f2c
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-06-28 21:38:43 +0000

    Fri, Jun 28, 2024, 5:38 PM -04:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -76,15 +76,16 @@ void free_family(person *p)
 {
 
     // TODO: Handle base case
-    if (p->parents[0] == NULL && p->parents[1] == NULL) {
-        free(p);
-    } else {
-    // TODO: Free parents recursively
-        free_family(p->parents[0]);
-        free_family(p->parents[1]);
+    if (p == NULL) {
+        return;
     }
+
+    // TODO: Free parents recursively
+    free_family(p->parents[0]);
+    free_family(p->parents[1]);
+
     // TODO: Free child
-    if (p != NULL) free(p);
+    free(p);
 }
 
 // Print each family member and their alleles.
