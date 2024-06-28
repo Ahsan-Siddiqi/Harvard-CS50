08f9392ae737410a3bff7df55edc3e625fe8194e
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-06-28 04:31:32 +0000

    Fri, Jun 28, 2024, 12:31 AM -04:00

--- recover/recover.c
+++ recover/recover.c
@@ -16,13 +16,13 @@ int main(int argc, char *argv[])
         exit(1)
     }
 
-
     // look through the raw for the jpeg pattern (0xff 0xd8 0xff)
 
         // iterate over chunks of 512 bytes or 1 block
     char buffer[512];
     size_t bytesRead;
     char pattern[] = {0xff, 0xd8, 0xff}
+    
 
     while(bytesRead = fread(&buffer, 1, sizeof(buffer), forensic)) {
 
