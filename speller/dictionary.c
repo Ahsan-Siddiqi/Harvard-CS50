79261dd9d88b8f0b79a7cb24f6f9c09885da7fb0
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-06-30 23:03:02 +0000

    Sun, Jun 30, 2024, 7:03 PM -04:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -30,7 +30,7 @@ bool check(const char *word)
     for (int i = 0; i < strlen(word); i++) {
         lowerWord[i] = tolower(word[i]);
     }
-    lowerWord[strlen(lowerWord)] = '\0';
+    lowerWord[strlen(word)] = '\0';
 
 
     unsigned int index = hash(lowerWord);
@@ -46,7 +46,7 @@ bool check(const char *word)
 
             if (strcmp(lowerWord, curNode->word) == 0) return true;
 
-            printf("lowerWord: %s\ncurnode->word: %s\n", lowerWord, curNode->word);
+            //printf("lowerWord: %s\ncurnode->word: %s\n", lowerWord, curNode->word);
 
             curNode = curNode->next;
         }
