261fb8f5891d8cbb234abf6416634e6e9ea81c31
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-06-30 02:09:26 +0000

    Sat, Jun 29, 2024, 10:09 PM -04:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -19,6 +19,17 @@ const unsigned int N = 1000;
 // Hash table
 node *table[N];
 
+// Custom implementation of strcasecmp
+int strcasecmp(const char *s1, const char *s2)
+{
+    while (*s1 && (tolower(*s1) == tolower(*s2)))
+    {
+        s1++;
+        s2++;
+    }
+    return tolower(*(unsigned char *)s1) - tolower(*(unsigned char *)s2);
+}
+
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
