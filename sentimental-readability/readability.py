8b62d7808580625f00046566ed689164b78f2306
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-07-01 19:26:50 +0000

    Mon, Jul 1, 2024, 3:26 PM -04:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,4 +1,15 @@
 text = input("Text: ")
 
-for words in text:
-    
+# compute L and S
+for char in text:
+
+# compute index
+index = 0.0588 * L - 0.296 * S - 15.8
+
+# print
+if index < 1:
+    print("Before Grade 1")
+elif index >= 16:
+    print("Grade 16+)
+else:
+    print("Grade " + index)
