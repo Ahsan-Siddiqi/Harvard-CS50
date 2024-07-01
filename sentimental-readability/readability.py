4e4bc449d89bcc9ae2376042cbc836826f1f4b6b
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-07-01 19:31:46 +0000

    Mon, Jul 1, 2024, 3:31 PM -04:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,7 +1,16 @@
 text = input("Text: ")
 
 # compute L and S
+punc = ["!", ".", "?"]
+sentances, chars = 0
+
 for char in text:
+    if char in punc:
+        sentances += 1
+    chars += 1
+
+L = chars / 100
+S = sentances / 100
 
 # compute index
 index = 0.0588 * L - 0.296 * S - 15.8
@@ -10,6 +19,6 @@ index = 0.0588 * L - 0.296 * S - 15.8
 if index < 1:
     print("Before Grade 1")
 elif index >= 16:
-    print("Grade 16+)
+    print("Grade 16+")
 else:
     print("Grade " + index)
