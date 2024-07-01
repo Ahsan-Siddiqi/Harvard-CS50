8df4a600fa0bf7a3f5f88d5a80e923701b88735d
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-07-01 21:48:21 +0000

    Mon, Jul 1, 2024, 5:48 PM -04:00

--- dna/dna.py
+++ dna/dna.py
@@ -31,7 +31,8 @@ def main():
     # TODO: Check database for matching profiles
     for row in csvRows:
         for str, n in row.items():
-            if n == strs[str]
+            if n == strs[str]:
+                print(row[name])
     return
 
 
