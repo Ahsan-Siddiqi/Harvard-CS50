21a9d6661eca75052bcf4100ac1d7c347952d1ba
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-07-01 15:15:34 +0000

    Mon, Jul 1, 2024, 11:15 AM -04:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -3,5 +3,12 @@ height = input("Height: ")
 num = height
 
 for i in height:
-    for j in height:
-        num += 1
+    for j in height * 2 + 1:
+        if j < height - i:
+            print(" " end='')
+        elif j == height - i:
+            print("#" end='')
+        
+
+    print("#" end='')
+    print()
