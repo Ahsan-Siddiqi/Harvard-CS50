497d97eea6e5a021b5d8ddff88a43d1360f473eb
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-07-03 16:00:25 +0000

    Wed, Jul 3, 2024, 12:00 PM -04:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,1 +1,1 @@
-SELECT title FROM movies JOIN ratings ON movies.id = ratings.movie_id
+SELECT movies.title, ratings.rating FROM movies JOIN ratings ON movies.id = ratings.movie_id WHERE movies.year = 2010 ORDER BY ratings.rating DESC, movies.title ASC;
