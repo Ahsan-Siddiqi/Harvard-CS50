bca6047cd2d436c11efbc25decd0dfe5d8a703a9
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-07-03 18:57:35 +0000

    Wed, Jul 3, 2024, 2:57 PM -04:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -3,4 +3,5 @@
 SELECT name FROM people WHERE passport_number IN (
     SELECT passport_number FROM passengers WHERE flight_id = 36)
     AND license_plate IN (
-        SELECT license_plate FROM bakery_security_logs WHERE );
+        SELECT license_plate FROM bakery_security_logs WHERE year = 2023 AND day = 28 AND hour = 10 AND minute > 15 AND minute < 25)
+    AND phone_number IN (SELECT );
