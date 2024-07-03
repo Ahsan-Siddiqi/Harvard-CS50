81f3bab6369299c94aecb9ba17ba748953a27c7d
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-07-03 15:42:00 +0000

    Wed, Jul 3, 2024, 11:42 AM -04:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,1 +1,1 @@
-SELECT name FROM people WHERE id IN (SELECT people_id FROM stars WHERE movie_id )
+SELECT name FROM people WHERE id IN (SELECT people_id FROM stars WHERE people_id = (SELECT id FROM people WHERE name = "Kevin Bacon"))
