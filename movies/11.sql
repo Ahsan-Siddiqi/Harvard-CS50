e502e5c3761937c97c26ebdeabf6c476bba8df89
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-07-03 16:09:47 +0000

    Wed, Jul 3, 2024, 12:09 PM -04:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,1 +1,1 @@
-SELECT title FROM movies WHERE id IN (SELECT movie_id FROM ratings WHERE movie_id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Chadwick Boseman")) ORDER BY rating ASC LIMIT 5);
+SELECT title FROM movies WHERE id IN (SELECT movie_id FROM ratings WHERE movie_id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Chadwick Boseman")) ORDER BY rating LIMIT 5);
