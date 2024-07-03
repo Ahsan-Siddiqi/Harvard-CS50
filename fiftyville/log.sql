073ac33a44d001356670e90adb5a5c0f3b7bea61
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-07-03 19:15:05 +0000

    Wed, Jul 3, 2024, 3:15 PM -04:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -12,3 +12,6 @@ SELECT name FROM people WHERE passport_number IN (
     AND license_plate IN (
         SELECT license_plate FROM bakery_security_logs WHERE year = 2023 AND day = 28 AND hour = 10 AND minute > 15 AND minute < 25)
     AND phone_number IN (SELECT caller FROM phone_calls WHERE year = 2023 AND day = 28 AND month = 7 AND duration < 60);
+
+--check who withdrew the money at leggett street
+SELECT name FROM people WHERE id IN (SELECT person_id FROM bank_accounts WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE year = 2023 AND month = 7 AND day = 28 AND transaction_type = "withdraw" AND atm_location = "Leggett Street"));
