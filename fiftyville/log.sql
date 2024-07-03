069352aa4fda5810ca83ab83f9df26e008304194
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-07-03 19:05:26 +0000

    Wed, Jul 3, 2024, 3:05 PM -04:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,7 +1,7 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
---find earlist flight
-SELECT id 
+--find earlist flight the day after 
+SELECT id FROM flights WHERE year = 2023 AND day = 29
 
 --using the flight id, license plate from the security, and the phone number the day of which was less that one minute long get a list of suspects
 SELECT name FROM people WHERE passport_number IN (
