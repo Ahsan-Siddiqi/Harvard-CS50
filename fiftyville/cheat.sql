7914fab440096081f22026c8d992c450bfc1a98d
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-07-03 17:31:05 +0000

    Wed, Jul 3, 2024, 1:31 PM -04:00

--- fiftyville/cheat.sql
+++ fiftyville/cheat.sql
@@ -1,11 +1,32 @@
 .mode csv
 .headers on
 
-.output table1.csv
-SELECT * FROM table1;
+.output airports.csv
+SELECT * FROM airports;
 
-.output table2.csv
-SELECT * FROM table2;
+.output crime_scene_reports.csv
+SELECT * FROM crime_scene_reports;
 
-.output tableN.csv
-SELECT * FROM tableN;
+.output people.csv
+SELECT * FROM people;
+
+.output atm_transactions.csv
+SELECT * FROM atm_transactions;
+
+.output flights.csv
+SELECT * FROM flights;
+
+.output phone_calls.csv
+SELECT * FROM phone_calls;
+
+.output bakery_security_logs.csv
+SELECT * FROM bakery_security_logs;
+
+.output interviews.csv
+SELECT * FROM interviews;
+
+.output bank_accounts.csv
+SELECT * FROM bank_accounts;
+
+.output passengers.csv
+SELECT * FROM passengers;
