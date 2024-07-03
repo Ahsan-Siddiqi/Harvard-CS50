fb5981701ba67bc10764b022c84a3b4ff70036f1
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-07-03 18:52:03 +0000

    Wed, Jul 3, 2024, 2:52 PM -04:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,3 +1,3 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
-SELECT name FROM people WHERE passport_number IN (SELECT passport_number FROM passengers WHERE flight_id = 36) AND license_plate IN (SELECT );
+SELECT name FROM people WHERE passport_number IN (SELECT passport_number FROM passengers WHERE flight_id = 36) AND license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE );
