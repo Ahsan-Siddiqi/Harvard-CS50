64564963f3ab3c80028c4e6cd3efff94ce6632b0
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-07-03 16:31:00 +0000

    Wed, Jul 3, 2024, 12:31 PM -04:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,11 +1,1 @@
-SELECT title FROM movies
-WHERE id IN (
-    SELECT movie_id
-    FROM stars
-    WHERE person_id = (SELECT id FROM people WHERE name = "Bradley Cooper")
-    )
-AND IN (
-    SELECT movie_id
-    FROM stars
-    WHERE person_id = (SELECT id FROM people WHERE name = "Jennifer Lawrence")
-    );
+SELECT title FROM movies WHERE id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Bradley Cooper")) AND id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Jennifer Lawrence"));
