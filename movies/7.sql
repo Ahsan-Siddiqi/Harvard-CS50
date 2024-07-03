4f0dc87cca5dd1d993ed70f61f6be628c73febf8
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-07-03 15:52:25 +0000

    Wed, Jul 3, 2024, 11:52 AM -04:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,1 +1,1 @@
-SELECT title FROM movies WHERE year = 2010 ORDER BY (SELECT rating FROM ratings) DESC;
+SELECT title FROM movies WHERE year = 2010 AND id IN (SELECT movie_id FROM ratings ORDER BY rating DESC);
