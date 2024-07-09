c4d7a84464dcec6c3e0b47989ff175587727b614
Ahsan-Siddiqi <166544128+Ahsan-Siddiqi@users.noreply.github.com>
2024-07-09 19:40:31 +0000

    Tue, Jul 9, 2024, 3:40 PM -04:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -31,7 +31,7 @@ def index():
         month = request.form.get("month")
         day = request.form.get("day")
 
-        
+        db.execute("INSERT INTO birthdays (name, month, sport) VALUES)
 
         return redirect("/")
 
