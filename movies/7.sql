7b4db3edd7ec2ff7ffc88241d8dbc795e4f771a7
Ahsan-Siddiqi <ahsanhsiddiqi@gmail.com>
2024-09-16 11:42:06 -0400

    gh-pages deployment

--- movies/7.sql
+++
@@ -1,1 +0,0 @@
-SELECT movies.title, ratings.rating FROM movies JOIN ratings ON movies.id = ratings.movie_id WHERE movies.year = 2010 ORDER BY ratings.rating DESC, movies.title ASC;
--- sentimental-hello/hello.py
+++
@@ -1,1 +0,0 @@
-print("hello, " + input("What is your name? "))
